2ad59f1deb748f0e3b3a60d6b24d706668ee7728
kyle7998 <14101951+kyle799@users.noreply.github.com>
2024-01-07 09:42:29 -0700

    Update splitter.py to support dynamic chunk size

--- splitter.py
+++ splitter.py
@@ -1,14 +1,18 @@
+#!/bin/python3
 import os
 
 def split_file(file_path, chunk_size):
     file_size = os.path.getsize(file_path)
     chunk_size_bytes = chunk_size * 1024 * 1024 * 1024  # Convert GB to bytes
+    chunk_size_bytes = int(chunk_size_bytes)
     num_chunks = file_size // chunk_size_bytes
+    num_chunks = (int(num_chunks))
 
     with open(file_path, 'rb') as file:
         for i in range(num_chunks):
             chunk_data = file.read(chunk_size_bytes)
             chunk_file_name = f'{file_path}.part{i+1}'
+            print(f"Chonking file {i+1} of {num_chunks+1} {round((i+1)/num_chunks*100)}% complete {' ' * 20}", end='\r')
             with open(chunk_file_name, 'wb') as chunk_file:
                 chunk_file.write(chunk_data)
 
@@ -21,9 +25,14 @@ def split_file(file_path, chunk_size):
 # Usage example
 def main():
     file_path = input("Enter the file path: ")
-    chunk_size_gb = 4.5
+    chunk_size_gb = input("Enter the file chunk size (in GB), default to 4.5 GB: ")
+    if chunk_size_gb == "":
+        chunk_size_gb = 4.5
+    else:
+        chunk_size_gb = float(chunk_size_gb)
+    
     split_file(file_path, chunk_size_gb)
 
-
-if main == "__main__":
+if __name__ == "__main__":
     main()
+    
